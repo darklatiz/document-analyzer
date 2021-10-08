5ed816f482e7e21e2ad6e845f22c7fbf9ed872a4
Spike Storm <software.j2ee@gmail.com>
2021-10-07 20:07:54 -0700

    Data Model in postgres and connection configuration,

--- src/main/java/mx/gigabyte/labs/document/analyzer/domain/model/Document.java
+++ src/main/java/mx/gigabyte/labs/document/analyzer/domain/model/Document.java
@@ -2,11 +2,17 @@ package mx.gigabyte.labs.document.analyzer.domain.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -15,4 +21,23 @@ public class Document {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "document_type", nullable = false, length = 50)
+  private DocumentType documentType;
+
+  @Column(name = "is_active")
+  private boolean isActive;
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
 }
--- src/main/java/mx/gigabyte/labs/document/analyzer/domain/model/User.java
+++ src/main/java/mx/gigabyte/labs/document/analyzer/domain/model/User.java
@@ -2,12 +2,16 @@ package mx.gigabyte.labs.document.analyzer.domain.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -15,11 +19,28 @@ import javax.persistence.Table;
 @Table(name = "user_profile")
 public class User {
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long profileId;
+
+  @Column(name = "name", nullable = false, length = 75)
   private String name;
+  @Column(name = "last_name", nullable = false, length = 150)
   private String lastName;
+
+  @Column(name = "user_name", nullable = false, unique = true, length = 30)
   private String userName;
+  @Column(name = "email", nullable = false, unique = true, length = 100)
   private String email;
+
+  @Column(name = "is_active")
+  private boolean isActive;
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
 
 }
